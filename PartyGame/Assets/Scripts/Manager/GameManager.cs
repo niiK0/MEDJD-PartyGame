6a5944288e6bbbf248042c175f3476e1ebea92f4
niiK0 <37949374+niiK0@users.noreply.github.com>
2024-11-22 17:14:54 +0000

    a few bugs fixed

--- PartyGame/Assets/Scripts/Manager/GameManager.cs
+++ PartyGame/Assets/Scripts/Manager/GameManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,6 +14,7 @@ public class GameManager : MonoBehaviour
     public MainMenuUI mainMenuUI;
 
     private List<PlayerMovement> players = new();
+    private Dictionary<int, bool> availableIDs = new Dictionary<int, bool>() { {1, true }, {2, true }};
 
     private void Awake()
     {
@@ -30,7 +32,7 @@ public class GameManager : MonoBehaviour
     {
     }
 
-    public void LeaveRoom(PlayerMovement player)
+    public void LeaveRoom(PlayerInput player)
     {
         //players.Remove(player);
         Destroy(player.gameObject);
@@ -41,8 +43,16 @@ public class GameManager : MonoBehaviour
         Debug.Log("Player left: " + playerInput.devices[0].displayName);
 
         if (!players.Contains(playerInput.GetComponent<PlayerMovement>())) return;
+        availableIDs[playerInput.GetComponent<PlayerMovement>().playerID] = true;
         players.Remove(playerInput.GetComponent<PlayerMovement>());
         mainMenuUI.SetPlayerReady(playerInput.GetComponent<PlayerMovement>().playerID, ControllerType.None, false);
+    }
+
+    public int FindAvailableID()
+    {
+        if(!availableIDs.ContainsValue(true)) return -1;
+
+        return availableIDs.First(x => x.Value == true).Key;
     }
 
     public void OnPlayerJoined(PlayerInput playerInput)
@@ -51,16 +61,23 @@ public class GameManager : MonoBehaviour
 
         if (players.Contains(playerInput.GetComponent<PlayerMovement>())) return;
 
+        int playerID = FindAvailableID();
+        if (playerID == -1) return;
+
         players.Add(playerInput.GetComponent<PlayerMovement>());
-        playerInput.GetComponent<PlayerMovement>().playerID = players.Count;
+        playerInput.GetComponent<PlayerMovement>().playerID = playerID;
+        availableIDs[playerID] = false;
 
         ControllerType controllerType = ControllerType.None;
 
-        if (playerInput.devices[0].displayName == "Keyboard")
+        InputDevice deviceType = InputSystem.devices.ToArray().ToList<InputDevice>().Find(x => x.deviceId == playerInput.devices[0].deviceId);
+
+        if (deviceType is Keyboard)
         {
             controllerType = ControllerType.KBM;
         }
-        else
+
+        if(deviceType is Gamepad)
         {
             controllerType = ControllerType.GPAD;
         }
@@ -71,6 +88,13 @@ public class GameManager : MonoBehaviour
     public void StartGame()
     {
         //START GAME HERE
+        //STOP CONTROLLER JOIN
+        mainMenuUI.GetComponent<UIDocument>().enabled = false;
+        inputManager.DisableJoining();
+        foreach (var player in players)
+        {
+            player.canLeave = false;
+        }
         Debug.Log("Game Started");
     }
 }
