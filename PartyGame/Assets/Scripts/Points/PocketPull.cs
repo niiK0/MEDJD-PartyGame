63701f0048efd7cad6cef514722cf0faf42769c2
niiK0 <37949374+niiK0@users.noreply.github.com>
2024-11-23 17:26:39 +0000

    Pocket and magnet pull, smoothing stuff

---
+++ PartyGame/Assets/Scripts/Points/PocketPull.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PocketPull : MonoBehaviour
+{
+    public float pullSpeed = 2.0f; // Speed of pulling effect
+    public float pullRadius = 5.0f; // Range within which the pull effect applies
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Point")) return;
+
+        var point = other.GetComponent<Point>();
+
+        if (point.holder != null) return;
+        if (point.isBeingPulled) return;
+
+        Debug.Log("Pulling " + other.name);
+
+        ApplyPull(point);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Point")) return;
+
+        var point = other.GetComponent<Point>();
+
+        point.StopPulling();
+    }
+
+    private void ApplyPull(Point point)
+    {
+        point.StartPullingTowards(transform.position, pullSpeed);
+    }
+}
