785d46f6f3a620dc8eefaf311f20ff6ddeae2a4f
niiK0 <37949374+niiK0@users.noreply.github.com>
2024-11-27 17:03:37 +0000

    player character animation head tilt and others

--- PartyGame/Assets/Scripts/Player/PlayerMovement.cs
+++ PartyGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -24,8 +24,14 @@ public class PlayerMovement : MonoBehaviour
 
     public Vector2 moveInput;
     public GameObject visuals;
+    public GameObject playerHead;
+    public GameObject playerHeadBlue;
+    public GameObject playerHeadOrange;
+    public Animator playerAnimator;
     public float tiltAmount = 1f;
     public float tiltAmountFront = 5f;
+    public float headUDTurnAmount = 20f;
+    public float headLRTurnAmount = 60f;
 
     public bool isGettingPushed = false;
 
@@ -62,12 +68,16 @@ public class PlayerMovement : MonoBehaviour
     {
         redVersion.SetActive(true);
         blueVersion.SetActive(false);
+        playerHead = playerHeadOrange;
+        playerAnimator = redVersion.GetComponentInChildren<Animator>();
     }
 
     public void EnableBlueVersion()
     {
         redVersion.SetActive(false);
         blueVersion.SetActive(true);
+        playerHead = playerHeadBlue;
+        playerAnimator = blueVersion.GetComponentInChildren<Animator>();
     }
 
     private void MovePlayer()
@@ -111,7 +121,7 @@ public class PlayerMovement : MonoBehaviour
     private void GetInteractInput()
     {
         float leaveInput = playerInput.actions["Interact"].ReadValue<float>();
-        if(leaveInput == 1)
+        if(leaveInput == 1 && !isGettingPushed)
         {
             if(canLeave)
                 PlayerLeave(playerInput);
@@ -163,6 +173,38 @@ public class PlayerMovement : MonoBehaviour
                 Quaternion.identity, // Neutral position
                 Time.deltaTime * 10f
             );
+        }
+
+        if (playerHead != null)
+        {
+            if(movementDirection != Vector3.zero)
+            {
+                if(playerAnimator.enabled)
+                    playerAnimator.enabled = false;
+
+                Quaternion headTargetRotation = Quaternion.Euler(
+                    -movementDirection.z * headUDTurnAmount,
+                    -movementDirection.x * headLRTurnAmount,
+                    0
+                );
+
+                playerHead.transform.localRotation = Quaternion.Lerp(
+                    playerHead.transform.localRotation,
+                    headTargetRotation,
+                    Time.deltaTime * 10f
+                );
+            }
+            else
+            {
+                playerHead.transform.localRotation = Quaternion.Lerp(
+                    playerHead.transform.localRotation,
+                    Quaternion.identity,
+                    Time.deltaTime * 10f
+                );
+
+                if(playerHead.transform.localRotation == Quaternion.identity)
+                    playerAnimator.enabled = true;
+            }
         }
     }
 }
